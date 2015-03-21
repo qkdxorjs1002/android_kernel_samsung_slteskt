6f66b3942e9279cd8055a13d24582f926210af42
qkdxorjs1002 <qkdxorjs1002@naver.com>
2015-03-22 02:24:38 +0900

    bring kernel source for slteskt from modpunk's

---
+++ drivers/w1/slaves/w1_ds28el15_sha256.h
@@ -0,0 +1,28 @@
+/*
+ * 1-Wire SHA256 software implementation for the ds23el15 chip
+ *
+ * Copyright (C) 2013 maximintergrated
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+
+#ifndef _W1_DS28EL15_SHA256_H
+#define _W1_DS28EL15_SHA256_H
+
+#include <linux/types.h>
+
+int compute_sha256(unchar* message, short length, ushort skipconst, ushort reverse, unchar* digest);
+int verify_mac256(unchar* MT, short lenght, unchar* compare_MAC);
+int compute_mac256(unchar* message, short length, unchar* MAC);
+int calculate_nextsecret256(unchar* binding, unchar* partial, int page_num, unchar* man_id);
+void set_secret(unchar *secret_data);
+void set_romid(unchar *romid_data);
+
+
+#endif
