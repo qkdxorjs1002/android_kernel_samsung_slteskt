990d94b6422237ec2a2a145947ffc68d1b1c7387
qkdxorjs1002 <qkdxorjs1002@naver.com>
2015-04-28 12:45:41 +0900

    Update Script

--- build_kernel.sh
+++
@@ -1,6 +0,0 @@
-##!/bin/bash
-CM_PATH=~/cm/prebuilts/gcc/darwin-x86/arm/arm-eabi-4.8
-export ARCH=arm
-export CROSS_COMPILE=$(CM_PATH)/bin/arm-eabi-
-make cm_exynos5430-slteskt_defconfig
-make -j10
---
+++ setenv.sh
@@ -0,0 +1,4 @@
+##!/bin/bash
+CM_PATH=~/cm12/prebuilts/gcc/darwin-x86/arm/arm-eabi-4.8
+export ARCH=arm
+export CROSS_COMPILE=$CM_PATH/bin/arm-eabi-
