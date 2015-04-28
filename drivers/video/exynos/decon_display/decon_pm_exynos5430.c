16749081215a59a7b6cef7b0a4726781942c50b8
qkdxorjs1002 <qkdxorjs1002@naver.com>
2015-04-28 12:44:42 +0900

    Decon-fb

--- drivers/video/exynos/decon_display/decon_pm_exynos5430.c
+++ drivers/video/exynos/decon_display/decon_pm_exynos5430.c
@@ -329,6 +329,8 @@ int init_display_decon_clocks(struct device *dev)
 		exynos_display_set_rate(dev, disp_pll, 67 * MHZ);
 	else if (lcd->xres * lcd->yres == 1080 * 1920)
 		exynos_display_set_rate(dev, disp_pll, 142 * MHZ);
+	else if (lcd->xres * lcd->yres == 1536 * 2048)
+		exynos_display_set_rate(dev, disp_pll, 214 * MHZ);
 	else if (lcd->xres * lcd->yres == 1440 * 2560)
 		exynos_display_set_rate(dev, disp_pll, 250 * MHZ);
 	else if (lcd->xres * lcd->yres == 2560 * 1600)
@@ -391,7 +393,7 @@ int init_display_decon_clocks(struct device *dev)
 		exynos_display_set_divide(dev, dout_sclk_dsim0_disp, 0);
 		exynos_display_set_parent(dev, mout_sclk_dsim0, "mout_sclk_dsim0_user");
 		exynos_display_set_parent(dev, mout_sclk_dsim0_user, "sclk_dsim0_disp");
-	} else if (lcd->xres * lcd->yres == 1440 * 2560) {
+	} else if (lcd->xres * lcd->yres == 1440 * 2560 || lcd->xres * lcd->yres == 1536 * 2048) {
 		exynos_display_set_divide(dev, dout_sclk_decon_eclk, 2);
 		exynos_display_set_parent(dev, mout_sclk_decon_eclk_c, "mout_sclk_decon_eclk_b");
 		exynos_display_set_parent(dev, mout_sclk_decon_eclk_b, "mout_sclk_decon_eclk_a");
@@ -576,12 +578,230 @@ static int set_pinctrl(struct device *dev, int enable)
 	return ret;
 }
 
+#if defined(CONFIG_DECON_LCD_S6TNMR7)
+int enable_display_driver_power(struct device *dev)
+{
+	struct display_driver *dispdrv = get_display_driver();
+	struct display_gpio *gpio = dispdrv->dt_ops.get_display_dsi_reset_gpio();
+	int ret = 0;
+
+	if (!num_supplies)
+		get_supplies(dev);
+
+	if (!num_supplies)
+		goto pin_config;
+
+	/* Turn on VDD_3.3V*/
+	ret = gpio_request_one(gpio->id[0], GPIOF_OUT_INIT_HIGH, "lcd_power");
+	if (ret < 0) {
+		pr_err("%s: gpio lcd_power request fail\n", __func__);
+		goto pin_config;
+	}
+	gpio_free(gpio->id[0]);
+
+	usleep_range(10000, 12000);
+
+	/* Turn on VDD_1.8V(TCON_1.8v) */
+	ret = regulator_enable(supplies[0].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[0].supply);
+		goto pin_config;
+	}
+
+pin_config:
+	set_pinctrl(dev, 1);
+
+	return 0;
+}
+
+int disable_display_driver_power(struct device *dev)
+{
+	struct display_driver *dispdrv;
+	struct display_gpio *gpio;
+	int ret;
+
+	dispdrv = get_display_driver();
+	gpio = dispdrv->dt_ops.get_display_dsi_reset_gpio();
+
+	if (!num_supplies)
+		get_supplies(dev);
+
+	if (!num_supplies)
+		goto pin_config;
+
+	/* Turn on VDD_1.8V(TCON_1.8v) */
+	ret = regulator_disable(supplies[0].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[0].supply);
+		goto pin_config;
+	}
+	usleep_range(5000, 10000);
+	/* Turn off VDD_3.3V*/
+	ret = gpio_request_one(gpio->id[0], GPIOF_OUT_INIT_LOW, "lcd_power");
+	if (ret < 0) {
+		pr_err("%s: gpio lcd_power request fail\n", __func__);
+		goto pin_config;
+	}
+	gpio_free(gpio->id[0]);
+
+pin_config:
+	set_pinctrl(dev, 0);
+
+	return 0;
+}
+
+int reset_display_driver_panel(struct device *dev)
+{
+	int timeout = 10;
+	int gpio;
+
+	pr_debug(" Chagall %s\n", __func__);
+
+	gpio = of_get_named_gpio(dev->of_node, "oled-pcd-gpio", 0);
+	if (gpio < 0) {
+		dev_err(dev, "failed to get proper gpio number\n");
+		return -EINVAL;
+	}
+	msleep_interruptible(150);
+	do {
+		if (gpio_get_value(gpio))
+			break;
+		msleep(30);
+	} while (timeout--);
+	if (timeout < 0)
+		pr_err(" %s timeout...\n", __func__);
+	else
+		pr_info("%s duration: %d\n", __func__, 150+(10-timeout)*30);
+	return 0;
+}
+#elif defined(CONFIG_DECON_LCD_S6E3HA1)
+int enable_display_driver_power(struct device *dev)
+{
+	int ret = 0;
+
+	if (!num_supplies)
+		get_supplies(dev);
+
+	if (!num_supplies)
+		goto pin_config;
+
+	/* Turn on VDDI(VDD3): 1.8 */
+	ret = regulator_enable(supplies[0].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[0].supply);
+		goto pin_config;
+	}
+
+	/* Wait 5ms */
+	usleep_range(5000, 6000);
+
+	/* Turn on VCI: 3.0 */
+	ret = regulator_enable(supplies[1].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[1].supply);
+		goto pin_config;
+	}
+
+	/* Turn on VDDR(VDDI_REG): 1.5 or 1.6 */
+	ret = regulator_enable(supplies[2].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[2].supply);
+		goto pin_config;
+	}
+
+	/* Wait 10ms */
+	usleep_range(10000, 11000);
+
+pin_config:
+	set_pinctrl(dev, 1);
+
+	return 0;
+}
+
+int disable_display_driver_power(struct device *dev)
+{
+	struct display_driver *dispdrv;
+	struct display_gpio *gpio;
+	int ret;
+
+	dispdrv = get_display_driver();
+	gpio = dispdrv->dt_ops.get_display_dsi_reset_gpio();
+
+	ret = gpio_request_one(gpio->id[0], GPIOF_OUT_INIT_LOW, "lcd_reset");
+	if (ret < 0) {
+		pr_err("Failed to get gpio number for the lcd_reset\n");
+		return -EINVAL;
+	}
+	gpio_free(gpio->id[0]);
+
+	if (!num_supplies)
+		get_supplies(dev);
+
+	if (!num_supplies)
+		goto pin_config;
+
+	/* Turn off VDDR(VDDI_REG): 1.5 or 1.6 */
+	ret = regulator_disable(supplies[2].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[2].supply);
+		goto pin_config;
+	}
+
+	/* Wait 5ms */
+	usleep_range(5000, 6000);
+
+	/* Turn off VCI: 3.0 */
+	ret = regulator_disable(supplies[1].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[1].supply);
+		goto pin_config;
+	}
+
+	/* Turn off VDDI(VDD3): 1.8 */
+	ret = regulator_disable(supplies[0].consumer);
+	if (ret < 0) {
+		pr_err("%s: regulator %s enable fail\n", __func__, supplies[0].supply);
+		goto pin_config;
+	}
+
+pin_config:
+	set_pinctrl(dev, 0);
+
+	return 0;
+}
+
+int reset_display_driver_panel(struct device *dev)
+{
+	struct display_driver *dispdrv;
+	struct display_gpio *gpio;
+
+	dispdrv = get_display_driver();
+
+	gpio = dispdrv->dt_ops.get_display_dsi_reset_gpio();
+	gpio_request_one(gpio->id[0], GPIOF_OUT_INIT_HIGH, "lcd_reset");
+	usleep_range(5000, 6000);
+	gpio_set_value(gpio->id[0], 0);
+	usleep_range(5000, 6000);
+	gpio_set_value(gpio->id[0], 1);
+	gpio_free(gpio->id[0]);
+
+	/* Wait 10ms */
+	usleep_range(10000, 11000);
+
+	return 0;
+}
+#else
 int enable_display_driver_power(struct device *dev)
 {
 #if !defined(CONFIG_MACH_XYREF5430) && !defined(CONFIG_MACH_ESPRESSO5433)
 	struct display_driver *dispdrv = get_display_driver();
 	struct display_gpio *gpio = dispdrv->dt_ops.get_display_dsi_reset_gpio();
 	int ret = 0, i;
+
+#ifdef CONFIG_LCD_ALPM
+	if (dispdrv->dsi_driver.dsim->lcd_alpm)
+		return 0;
+#endif
 
 	if (!num_supplies)
 		get_supplies(dev);
@@ -687,6 +907,7 @@ int disable_display_driver_power(struct device *dev)
 			goto pin_config;
 		}
 		gpio_free(gpio->id[1]);
+		usleep_range(3000, 5000);
 #endif
 	}
 	/* Turn off VDDI(VDD3): 1.8 */
@@ -727,6 +948,7 @@ int reset_display_driver_panel(struct device *dev)
 
 	return 0;
 }
+#endif
 
 int enable_display_decon_runtimepm(struct device *dev)
 {
@@ -750,7 +972,7 @@ int enable_display_dsd_clocks(struct device *dev, bool enable)
 			return -EBUSY;
 		}
 	}
-#if defined(CONFIG_SOC_EXYNOS5433)
+#if defined(CONFIG_SOC_EXYNOS5433) && !defined(CONFIG_DECON_LCD_S6TNMR7)
 	if (!dispdrv->decon_driver.gate_dsd_clk) {
 		dispdrv->decon_driver.gate_dsd_clk = clk_get(dev, "gate_dsd_clk");
 		if (IS_ERR(dispdrv->decon_driver.gate_dsd_clk)) {
@@ -773,7 +995,7 @@ int disable_display_dsd_clocks(struct device *dev, bool enable)
 		return -EBUSY;
 
 	clk_disable_unprepare(dispdrv->decon_driver.dsd_clk);
-#if defined(CONFIG_SOC_EXYNOS5433)
+#if defined(CONFIG_SOC_EXYNOS5433) && !defined(CONFIG_DECON_LCD_S6TNMR7)
 	if (dispdrv->decon_driver.gate_dsd_clk)
 		clk_disable_unprepare(dispdrv->decon_driver.gate_dsd_clk);
 #endif
