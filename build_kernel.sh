a5eca3299a8aed875afa27fbd5f1b7c7f1234234
qkdxorjs1002 <qkdxorjs1002@naver.com>
2015-05-01 07:13:53 +0900

    add defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
+CM_PATH=~/cm12.1/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
-
-make slteskt_defconfig
-
-make
+export CROSS_COMPILE=$CM_PATH/bin/arm-eabi-
